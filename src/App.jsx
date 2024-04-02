1cf003a5f05067beb02065e571c44d8c7bab6d82
Rajeev Sharma <rajeevsharmamachphy@gmail.com>
2024-04-02 13:31:09 +0530

    fixed background

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Routing from './components/Routing'
 
 const App = () => {
   return (
-    <div className='bg-gray-900 max-h-full md:h-screen mx:h-screen'>
+    <div className='bg-gray-900 max-h-full  mx:h-screen'>
       <Routing/>
     </div>
   )
